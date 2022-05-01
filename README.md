233de32810e09d40cc6c2dac4d6a1e6584648b56
G Chow <grendon.chow@gmail.com>
2022-05-01 16:19:19 +0800

    Added To do list in Readme

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # todo-list
+
+# A To Do list for my To Do list app
+-Save/load from local storage
+-List of projects objects, each project object has a list of Todo objects
+-Main Todo page should iterate through each project and ToDos
+-Need a way to keep track of curretnly selected tab
+-Tab switching
+-A way to edit and delete a project, including update project function
+-May need to use Modals/hidden/dropdown form to add details
+-Add/delete/edit tasks as well as update the display
--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 console.log("test");
+
 const butt = document.querySelector("#add-button");
 butt.onclick = () =>{
-    const test = document.createElement('li');
-    test.classList.add('projects')
-    test.textContent = "test";
-    const test2 = document.createElement('div');
-    test2.classList.add('todo-count');
-    const container = document.querySelector('.sidebar');
-    test.appendChild(test2);
-    container.appendChild(test);
+    const project = document.createElement('li');
+    project.classList.add('projects')
+    project.textContent = "test";
+    const count = document.createElement('div');
+    count.classList.add('todo-count');
+    const sidebar = document.querySelector('.sidebar');
+    project.appendChild(test2);
+    sidebar.appendChild(test);
 };
