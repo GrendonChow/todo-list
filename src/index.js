32fb9e2319a5a82029eb0d9d50a5d57c7f132208
G Chow <grendon.chow@gmail.com>
2022-05-01 16:06:28 +0800

    refactored styles and html

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 console.log("test");
-const test = document.createElement('div');
-test.classList.add('side-heading')
-test.innerHTML = "test";
-const test2 = document.createElement('div');
-test2.classList.add('todo-count');
-const list = document.createElement('li');
-const container = document.querySelector('.projects-container');
-list.appendChild(test);
-list.appendChild(test2);
-container.appendChild(list);
+const butt = document.querySelector("#add-button");
+butt.onclick = () =>{
+    const test = document.createElement('li');
+    test.classList.add('projects')
+    test.textContent = "test";
+    const test2 = document.createElement('div');
+    test2.classList.add('todo-count');
+    const container = document.querySelector('.sidebar');
+    test.appendChild(test2);
+    container.appendChild(test);
+};
