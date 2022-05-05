2ee6fef5e144dcfaa8e0a37fd6ac40c2edbf1aa4
G Chow <grendon.chow@gmail.com>
2022-05-05 18:47:04 +0800

    Created project object

---
+++ src/modules/project.js
@@ -0,0 +1,31 @@
+export default class Project {
+    constructor(name)
+    {
+        this.name = name;
+        this.todo = [];
+    }
+
+    addTodo(todo){
+        this._todo.push(todo);
+    }
+
+    removeTodo(index){
+        this._todo.splice(index, 1);
+    }
+
+    get todo(){
+        return this._todo;
+    }
+
+    get name(){
+        return this._name;
+    }
+
+    set name(name){
+        this._name = name;
+    }
+
+    set todo(todo){
+        this._todo = todo;
+    }
+}
